b507e5d922fd416063293d3242b609dbde3a3586
Thorsten Hoeger <thorsten.hoeger@hoegernet.com>
2016-04-01 18:04:52 +0200

    use annotation instead of proxy factory

--- interconnect/demo/src/main/java/de/taimos/dvalin/interconnect/demo/Client.java
+++ interconnect/demo/src/main/java/de/taimos/dvalin/interconnect/demo/Client.java
@@ -9,9 +9,9 @@ package de.taimos.dvalin.interconnect.demo;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,32 +22,28 @@ package de.taimos.dvalin.interconnect.demo;
 
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import de.taimos.daemon.spring.annotations.ProdComponent;
-import de.taimos.dvalin.interconnect.core.spring.DaemonProxyFactory;
+import de.taimos.dvalin.interconnect.core.daemon.Interconnect;
 import de.taimos.dvalin.interconnect.demo.api.IUserService;
 import de.taimos.dvalin.interconnect.demo.api.UserError;
 import de.taimos.dvalin.interconnect.demo.model.UserIVO_v1;
 import de.taimos.dvalin.interconnect.demo.model.requests.CreateUserIVO_v1;
-import de.taimos.dvalin.interconnect.demo.model.requests.FindUserByIdIVO_v1;
 import de.taimos.dvalin.interconnect.demo.model.requests.FindUserIVO_v1;
 import de.taimos.dvalin.interconnect.model.service.DaemonError;
 
 @ProdComponent
 public class Client {
 
-    @Autowired
-    private DaemonProxyFactory proxy;
+    @Interconnect
+    private IUserService svc;
 
     @Scheduled(initialDelay = 1000, fixedRate = 60000)
     public void init() {
-        IUserService svc = proxy.create(IUserService.class);
-
         try {
             System.out.println("Users:");
-            for (UserIVO_v1 user : svc.findUsers(new FindUserIVO_v1.FindUserIVO_v1Builder().build()).getElements()) {
+            for (UserIVO_v1 user : this.svc.findUsers(new FindUserIVO_v1.FindUserIVO_v1Builder().build()).getElements()) {
                 System.out.println("  - " + user.getId() + " " + user.getName());
             }
 
@@ -59,18 +55,18 @@ public class Client {
                 b.withName(name);
                 UserIVO_v1 user = b.build();
                 System.out.println("Creating user: " + name);
-                UserIVO_v1 cUser = svc.createUser(new CreateUserIVO_v1.CreateUserIVO_v1Builder().withValue(user).build());
+                UserIVO_v1 cUser = this.svc.createUser(new CreateUserIVO_v1.CreateUserIVO_v1Builder().withValue(user).build());
                 System.out.println("Created user: " + cUser.getName() + " with id " + cUser.getId());
             }
 
             System.out.println();
 
             System.out.println("Users:");
-            for (UserIVO_v1 user : svc.findUsers(new FindUserIVO_v1.FindUserIVO_v1Builder().build()).getElements()) {
+            for (UserIVO_v1 user : this.svc.findUsers(new FindUserIVO_v1.FindUserIVO_v1Builder().build()).getElements()) {
                 System.out.println("  - " + user.getId() + " " + user.getName());
             }
 
-            svc.findById(new FindUserByIdIVO_v1.FindUserByIdIVO_v1Builder().withId("1000").build());
+//            svc.findById(new FindUserByIdIVO_v1.FindUserByIdIVO_v1Builder().withId("1000").build());
         } catch (DaemonError daemonError) {
             if (daemonError.getNumber().equals(UserError.USER_NOT_FOUND)) {
                 System.out.println("User does not exist");
