08f55c0e0d319b95eded07dda0bdbd4ba6f3c705
Thorsten Hoeger <thorsten.hoeger@hoegernet.com>
2016-05-27 10:12:21 +0200

    fix #19 by implementing ISpringLifecycleListener

---
+++ daemon/src/main/java/de/taimos/dvalin/daemon/SpringLifecycleAdapter.java
@@ -0,0 +1,34 @@
+package de.taimos.dvalin.daemon;
+
+public class SpringLifecycleAdapter implements ISpringLifecycleListener {
+
+    @Override
+    public void afterContextStart() {
+
+    }
+
+    @Override
+    public void started() {
+
+    }
+
+    @Override
+    public void stopping() {
+
+    }
+
+    @Override
+    public void beforeContextStop() {
+
+    }
+
+    @Override
+    public void aborting() {
+
+    }
+
+    @Override
+    public void signalUSR2() {
+
+    }
+}
