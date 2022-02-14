3ac5172947c17d1e84b84ba066bb7c635bf5888d
Marc Weise <marc.weise@cinovo.de>
2022-02-14 11:02:02 +0100

    Fix PowerMock JMX issue

--- test/src/main/java/de/taimos/dvalin/test/jaxrs/AbstractJaxRsPowermockTest.java
+++ test/src/main/java/de/taimos/dvalin/test/jaxrs/AbstractJaxRsPowermockTest.java
@@ -20,16 +20,17 @@ package de.taimos.dvalin.test.jaxrs;
  * #L%
  */
 
+import de.taimos.daemon.log4j.Log4jLoggingConfigurer;
 import org.apache.cxf.jaxrs.utils.JAXRSUtils;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import de.taimos.daemon.log4j.Log4jLoggingConfigurer;
-
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({JAXRSUtils.class})
+@PowerMockIgnore("javax.management.*")
 public class AbstractJaxRsPowermockTest {
 
     @BeforeClass
